8977d63243a936c1db6496075af3b5b5e6909b40
Efrain Rochin <erochin@h1webstudio.com>
2015-09-22 16:26:25 -0600

    Modificacion del App_helper para que se mantenga el parametro mobile en todos las urls

--- src/app/app_helper.php
+++ src/app/app_helper.php
@@ -2,12 +2,19 @@
 class AppHelper extends Helper{
 
 	function url($url = null, $full = false) {
+		$params = Router::getParams();
+		if(isset($params['url']['mobile'])){
+			$params = "?mobile=".$params['url']['mobile'];
+		}else{
+			$params = "";
+		}
 		if(is_string($url)){
-			return Router::url($url, $full);
+			return Router::url($url, $full).$params;
 		}
 		$url=Router::url($url, $full);
+
 		if(!preg_match('/#.+/', $url)){
-			return preg_replace('/\/$/','',Router::url($url, $full)).'/';
+			return preg_replace('/\/$/','',Router::url($url, $full)).'/'.$params;
 		}
 		return preg_replace('/#/','/#',$url);
 	}
